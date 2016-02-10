445e563b478bc394ec40f0b2248f0b76642431d6
Stanley Hansen <stanley321@gmail.com>
2016-02-09 18:48:34 -0800

    Change to use PATCH and controller action
    Controller action and form action are different entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :requests
 
-  post '/requests/:id/mark' => 'requests#mark', as: :mark_request
+  patch '/requests/:id/mark' => 'requests#mark', as: :mark_request
 
   root 'requests#index'
   # The priority is based upon order of creation: first created -> highest priority.
